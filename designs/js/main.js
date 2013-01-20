2214b0812d0c4f88b70384d23c8fc084341c98e0
Teqqles <david.long@racing-leagues.co.uk>
2013-01-20 19:48:40 +0000

    Merged javascript into separate file to remove duplication

---
+++ designs/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".text").focus(function(src) {
+        if ($(this).val() == $(this)[0].title) {
+            $(this).removeClass("defaultTextActive");
+            $(this).val("");
+        }
+    });
+
+    $(".text").blur(function() {
+        if ($(this).val() == "") {
+            $(this).addClass("defaultTextActive");
+            $(this).val($(this)[0].title);
+        }
+    });
+
+    $(".text").blur();
+});
